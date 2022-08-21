71ebf04604ad6afa6fa2288a0dff2c9dcbba4026
kirin765 <asipalseri@gmail.com>
2022-08-21 13:05:56 +0900

    Lombok 및 HelloResponseDto

--- src/main/java/org/example/springboot/web/HelloController.java
+++ src/main/java/org/example/springboot/web/HelloController.java
@@ -1,7 +1,10 @@
 package org.example.springboot.web;
 
+import org.example.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloController {
@@ -9,6 +12,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 
 }
